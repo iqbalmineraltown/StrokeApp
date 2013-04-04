91c583e8c908d50e31188eceb7040fe844e1c61d
Muhammad Febrian Ramadhana <febrian.12@gmail.com>
2013-04-04 13:12:09 +0700

    tambah library andengine GLES2

--- src/org/ristek/strokeapp/MainActivity.java
+++ src/org/ristek/strokeapp/MainActivity.java
@@ -1,22 +1,79 @@
 package org.ristek.strokeapp;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.menu.MenuScene;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SimpleBaseGameActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	// ===========================================================
+	// Constants
+	// ===========================================================
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
+	private static final int CAMERA_WIDTH = 800;
+	private static final int CAMERA_HEIGHT = 480;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	protected Camera mCamera;
+	
+	protected MenuScene mMenuScene;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		RatioResolutionPolicy resolutionPolicy = new RatioResolutionPolicy(
+				CAMERA_HEIGHT, CAMERA_HEIGHT);
+
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED,
+				resolutionPolicy, mCamera);
+	}
+
+	@Override
+	protected void onCreateResources() {
+		// TODO: Load Resource disini
+	}
+
+	@Override
+	protected Scene onCreateScene() {
+		this.mEngine.registerUpdateHandler(new FPSLogger());	
+		
+		mMenuScene = this.createMenuScene();
+		
+		return mMenuScene;
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	public MenuScene createMenuScene(){
+		MenuScene menuScene = new MenuScene(this.mCamera);
+		
+		return menuScene;
+	}
+	
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+
 }
