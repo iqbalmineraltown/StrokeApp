e11c1a94d61a6ba579b4329bbaf109608d77eced
Muhammad Febrian Ramadhana <febrian.12@gmail.com>
2013-04-06 15:26:42 +0700

    tambah main menu dan menu itemnya

--- src/org/ristek/strokeapp/MainActivity.java
+++ src/org/ristek/strokeapp/MainActivity.java
@@ -5,9 +5,21 @@ import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.scene.menu.MenuScene;
+import org.andengine.entity.scene.menu.item.TextMenuItem;
+import org.andengine.entity.text.Text;
+import org.andengine.entity.text.TextOptions;
 import org.andengine.entity.util.FPSLogger;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.font.FontFactory;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.util.HorizontalAlign;
+import org.andengine.util.color.Color;
+
+import android.graphics.Typeface;
 
 public class MainActivity extends SimpleBaseGameActivity {
 
@@ -18,13 +30,20 @@ public class MainActivity extends SimpleBaseGameActivity {
 	private static final int CAMERA_WIDTH = 800;
 	private static final int CAMERA_HEIGHT = 480;
 
+	protected static final int MENU_START = 0;
+	protected static final int MENU_TIME_TRIAL = 1;
+	protected static final int MENU_HIGH_SCORE = 2;
+	protected static final int MENU_STROKE_FACT = 3;
+	protected static final int MENU_OPTIONS = 4;
+	
 	// ===========================================================
 	// Fields
 	// ===========================================================
 
 	protected Camera mCamera;
-	
-	protected MenuScene mMenuScene;
+	protected Scene mMenuScene;
+
+	private Font mFont;
 
 	// ===========================================================
 	// Constructors
@@ -41,7 +60,7 @@ public class MainActivity extends SimpleBaseGameActivity {
 	@Override
 	public EngineOptions onCreateEngineOptions() {
 		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-		RatioResolutionPolicy resolutionPolicy = new RatioResolutionPolicy(
+		final RatioResolutionPolicy resolutionPolicy = new RatioResolutionPolicy(
 				CAMERA_WIDTH, CAMERA_HEIGHT);
 
 		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED,
@@ -50,15 +69,18 @@ public class MainActivity extends SimpleBaseGameActivity {
 
 	@Override
 	protected void onCreateResources() {
-		// TODO: Load Resource disini
+		this.mFont = FontFactory.create(this.getFontManager(),
+				this.getTextureManager(), 256, 256,
+				Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 32);
+		this.mFont.load();
 	}
 
 	@Override
 	protected Scene onCreateScene() {
-		this.mEngine.registerUpdateHandler(new FPSLogger());	
-		
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+
 		mMenuScene = this.createMenuScene();
-		
+
 		return mMenuScene;
 	}
 
@@ -66,12 +88,40 @@ public class MainActivity extends SimpleBaseGameActivity {
 	// Methods
 	// ===========================================================
 
-	public MenuScene createMenuScene(){
-		MenuScene menuScene = new MenuScene(this.mCamera);
+	public Scene createMenuScene() {
+		final Scene menuScene = new Scene();
+		menuScene.setBackground(new Background(Color.WHITE));
+
+		final VertexBufferObjectManager VBOManager = this
+				.getVertexBufferObjectManager();
+
+		
+		final Text titleText = new Text(100, 40, this.mFont, "Stroke Game",
+				new TextOptions(HorizontalAlign.LEFT), VBOManager);
+		
+		final String[] menuText = {"Start","Time Trial","High Score","Stroke Fact","Options"};
+		
+		final Text[] menuItem = new Text[menuText.length];
+		
+		for (int i = 0; i < menuItem.length; i++) {
+			menuItem[i] = new Text(100,100+(i*40),this.mFont,menuText[i],VBOManager){
+				@Override
+				public boolean onAreaTouched(TouchEvent pSceneTouchEvent,
+						float pTouchAreaLocalX, float pTouchAreaLocalY) {
+					System.out.println(this.getText()+" ditekan!");
+					return super
+							.onAreaTouched(pSceneTouchEvent, pTouchAreaLocalX, pTouchAreaLocalY);
+				}
+			};
+			menuScene.attachChild(menuItem[i]);
+			menuScene.registerTouchArea(menuItem[i]);
+		}
+		
+		menuScene.attachChild(titleText);
 		
 		return menuScene;
 	}
-	
+
 	// ===========================================================
 	// Inner and Anonymous Classes
 	// ===========================================================
