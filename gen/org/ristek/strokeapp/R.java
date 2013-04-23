669f10ab518d7ff28327e61ef558c968eb985f46
iqbalmineraltown <iqbalmineraltown@gmail.com>
2013-04-23 21:14:53 +0700

    coba masang cerita

--- gen/org/ristek/strokeapp/R.java
+++ gen/org/ristek/strokeapp/R.java
@@ -14,22 +14,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int linearbar=0x7f070005;
-        public static final int menu_settings=0x7f070006;
+        public static final int content=0x7f070005;
         public static final int myfivepanelpager=0x7f070000;
-        public static final int text=0x7f070004;
-        public static final int textview=0x7f070002;
-        public static final int topbar=0x7f070001;
-        public static final int view01=0x7f070003;
+        public static final int scroll=0x7f070001;
+        public static final int textview=0x7f070003;
+        public static final int tips=0x7f070006;
+        public static final int topbar=0x7f070002;
+        public static final int view01=0x7f070004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fact_page=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int main2=0x7f030003;
-        public static final int main3=0x7f030004;
-        public static final int main4=0x7f030005;
-        public static final int main5=0x7f030006;
+        public static final int story1=0x7f030002;
+        public static final int story2=0x7f030003;
+        public static final int story3=0x7f030004;
+        public static final int story4=0x7f030005;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
@@ -38,6 +37,18 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int story1_content=0x7f040004;
+        public static final int story1_tips=0x7f040005;
+        public static final int story1_title=0x7f040003;
+        public static final int story2_content=0x7f040007;
+        public static final int story2_tips=0x7f040008;
+        public static final int story2_title=0x7f040006;
+        public static final int story3_content=0x7f04000a;
+        public static final int story3_tips=0x7f04000b;
+        public static final int story3_title=0x7f040009;
+        public static final int story4_content=0x7f04000d;
+        public static final int story4_tips=0x7f04000e;
+        public static final int story4_title=0x7f04000c;
     }
     public static final class style {
         /** 
