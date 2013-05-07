13e7d8947ed3c5b06886bdf6200c7b5e32367ad5
iqbalmineraltown <iqbalmineraltown@gmail.com>
2013-05-08 01:42:08 +0700

    scrolling story udah kece tapi belom disambungin
    highscore tinggal pake sharedpreferences "highScore" dan kisah
    inspiratif harusnya udah bisa di-scroll

---
+++ src/org/ristek/strokeapp/OpeningActivity.java
@@ -0,0 +1,45 @@
+package org.ristek.strokeapp;
+
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.ScrollView;
+import android.widget.Scroller;
+import android.widget.TextView;
+
+public class OpeningActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.opening_story);
+		
+	   TextView tv = (TextView) findViewById(R.id.content_opening);
+	   ScrollView sv = (ScrollView) findViewById(R.id.scroll_opening);
+	   
+	   Scroller s = new Scroller(this,  new LinearInterpolator());
+	   tv.setScroller(s);
+	   s.startScroll(0, 0, 0, 100, 10000);
+	   /**
+	    * if s.isFinished -> pindah ke select stage
+	    */
+	   //sv.scrollTo(0, tv.getHeight());
+	   // tv.setMovementMethod(new ScrollingMovementMethod());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
